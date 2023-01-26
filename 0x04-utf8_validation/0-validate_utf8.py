c5d5f68302243636e9bd1b3d5172d09091c4a3c0
Karl Kamakia <karlkamakia@gmail.com>
2023-01-26 09:47:41 +0300

    Update 0-validate_utf8.py

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -6,42 +6,25 @@ data set represents a valid UTF-8 encoding
 
 
 def validUTF8(data):
-    """
-    method that determines if a given data
-    set represents a valid UTF-8 encoding
-    """
-    if data == [467, 133, 108]:
-        return True
-
-    remaining_bytes = 0
-
-    for byte in data:
-        if remaining_bytes > 0:
-            bin_byte = bin(byte)[2:].rjust(8, "0")
-
-            if bin_byte[:2] != "10":
-                return False
-
-            remaining_bytes -= 1
-
-        else:
-            bin_byte = bin(byte)[2:].rjust(8, "0")
-            if len(bin_byte) == 8 and bin_byte[0] == "0":
-                continue
-
-            elif len(bin_byte) == 8 and bin_byte[:3] == "110":
-                remaining_bytes = 1
-
-            elif len(bin_byte) == 8 and bin_byte[:4] == "1110":
-                remaining_bytes = 2
-
-            elif len(bin_byte) == 8 and bin_byte[:5] == "11110":
-                remaining_bytes = 3
-
+    i = 0
+    while i < len(data):
+        # Get the number of leading 1 bits in the first byte
+        num_bytes = 0
+        for j in range(7, -1, -1):
+            if data[i] & (1 << j) != 0:
+                num_bytes += 1
             else:
-                return False
-
-    if remaining_bytes > 0:
-        return False
-
+                break
+        # If the number of leading 1 bits is 0, this is a single-byte character
+        if num_bytes == 0:
+            i += 1
+        # If the number of leading 1 bits is 1, this is an invalid UTF-8 character
+        elif num_bytes == 1:
+            return False
+        # Otherwise, this is a multi-byte character, so check the following bytes
+        else:
+            for j in range(1, num_bytes):
+                if i + j >= len(data) or (data[i + j] & (1 << 7)) == 0 or (data[i + j] & (1 << 6)) != 0:
+                    return False
+            i += num_bytes
     return True
